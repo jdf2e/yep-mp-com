99ab2387d1149cfbd9432045e0a1abde6d1b0768
zhoujinxiu <zhoujinxiu@jd.com>
2019-01-10 18:08:57 +0800

    下拉菜单问题修复 回到顶部代码优化

--- src/dropMenu/index.js
+++ src/dropMenu/index.js
@@ -1,46 +1,44 @@
 Component({
-    properties:{
-        labelTxt: {
-            type: String,
-            value: '分类：'
-        },
-        isDropMenu:{
-            type: Boolean,
-            value: false
-        },
-        dropMenuData:{
-            type: Array,
-            value: [{val: '我是选择1', isSel: false}, {val: '我是选择2', isSel: false}, {val: '我是选择3', isSel: false}]
-        },
-        selVal:{
-            type: String,
-            value: '请选择类型'
-        },
-        curIndex: {
-            type: Number,
-            value: ''
-        },
-        icon:{
-            type: String,
-            value: 'http://img20.360buyimg.com/uba/jfs/t20959/196/687084072/15587/b595f4fa/5b14e462N1b76c89a.png'
-        }
+  properties: {
+    labelTxt: {
+      type: String,
+      value: '分类：'
     },
-    methods: {
-        select: function (e) {
-            console.log(e,'e')
-            let selValue = e.currentTarget.dataset.item.val;
-            let curIndex = e.currentTarget.dataset.index;
-            this.setData({
-            selVal: selValue,
-            curIndex: curIndex,
-            isDropMenu: false
-        });
-        },
-        isDropMenuShow: function () {
-            console.log(this,'this')
-            this.setData({
-                isDropMenu: !this.data.isDropMenu
-            })
-        }
+    isDropMenu: {
+      type: Boolean,
+      value: false
+    },
+    dropMenuData: {
+      type: Array,
+      value: [{val: '我是选择1', isSel: false}, {val: '我是选择2', isSel: false}, {val: '我是选择3', isSel: false}]
+    },
+    selVal: {
+      type: String,
+      value: '请选择类型'
+    },
+    curIndex: {
+      type: Number,
+      value: ''
+    },
+    icon: {
+      type: String,
+      value: 'http://img20.360buyimg.com/uba/jfs/t20959/196/687084072/15587/b595f4fa/5b14e462N1b76c89a.png'
     }
+  },
+  methods: {
+    select: function (e) {
+      let selValue = e.currentTarget.dataset.item.val;
+      let curIndex = e.currentTarget.dataset.index;
+      this.setData({
+        selVal: selValue,
+        curIndex: curIndex,
+        isDropMenu: false
+      });
+    },
+    isDropMenuShow: function () {
+      this.setData({
+        isDropMenu: !this.data.isDropMenu
+      })
+    }
+  }
 })
