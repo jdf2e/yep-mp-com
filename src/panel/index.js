1fedfba2d6e84300f993d786e70ff283962b254f
[liuxinjin@jd.com] <lxjqq627@126.com>
2019-06-26 17:46:48 +0800

    panel面板 组件 提交

---
+++ src/panel/index.js
@@ -0,0 +1,35 @@
+// components/panel/panel.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: ''
+    },
+    // 标题顶部距离
+    hideTop: {
+      type: Boolean,
+      value: false
+    },
+    hideBorder: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
